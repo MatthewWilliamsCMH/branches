2f9da9872262a339d15130ee4a868519b48ed4ea
jameswhatcott <james@whatcott.com>
2024-10-03 17:46:05 -0500

    added a seed file and an env file, still working to see if it'll work

--- server/models/Person.js
+++ server/models/Person.js
@@ -4,14 +4,19 @@ const Schema = mongoose.Schema;
 
 const PersonSchema = new Schema({
   firstName: { type: String, required: true },
-  middleName: { type: String, required: true},
+  middleName: { type: String, required: true },
   lastName: { type: String, required: true },
   dateOfBirth: { type: Date, required: true },
-  dateOfDeath: { type: Date, required: true },
+  dateOfDeath: { type: Date },
   gender: { type: String, required: true },
   birthPlace: { type: String, required: true },
-  burialSite: { type: String, required: true }
-
+  burialSite: { type: String },
+  parents: [{ type: Schema.Types.ObjectId, ref: 'Person' }] // Changed to array of ObjectId references
+}, {
+  timestamps: true
 });
+
+// Create an index on firstName and lastName for efficient searching
+PersonSchema.index({ firstName: 1, lastName: 1 });
 
 module.exports = mongoose.model('Person', PersonSchema);
