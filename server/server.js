8ad2791ffe41c55ee5487157d2bb1a5428f7388f
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-10-14 17:20:31 -0400

    again

--- server/server.js
+++ server/server.js
@@ -44,13 +44,13 @@ const startApolloServer = async () => {
   app.use(express.static(path.join(__dirname, 'public')));
 
   // if we're in production, serve client/dist as static assets
-  // if (process.env.NODE_ENV === 'production') {
-  //   app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  //   app.get('*', (req, res) => {
-  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
-  //   });
-  // } 
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  } 
 
   db.once('open', () => {
     app.listen(PORT, () => {
