3e8007eee62d12c7c449f2a50e3a4e2abcc7e2bf
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-10-14 15:44:43 -0400

    again

--- server/server.js
+++ server/server.js
@@ -3,16 +3,16 @@ const multer = require('multer');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
-// const cors = require('cors');
+const cors = require('cors');
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-// require('dotenv').config();
+require('dotenv').config();
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -43,14 +43,15 @@ const startApolloServer = async () => {
   app.use('/graphql', expressMiddleware(server));
   app.use(express.static(path.join(__dirname, 'public')));
 
-  app.get('/', (req, res) => {
-    res.send('Welcome to the API');
-  });
-
+  // if we're in production, serve client/dist as static assets
   // if (process.env.NODE_ENV === 'production') {
   //   app.use(express.static(path.join(__dirname, '../client/dist')));
-  // }
-  
+
+  //   app.get('*', (req, res) => {
+  //     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  //   });
+  // } 
+
   db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
