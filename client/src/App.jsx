8d52e50a0673f3e127108ebf19e6f63a68cce7be
Bluf00 <realmichaelnyamohanga@gmail.com>
2024-10-16 19:06:39 -0400

    working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 // client/src/App.jsx
 import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, gql,createHttpLink } from '@apollo/client';
 import {setContext} from '@apollo/client/link/context'
+import { Outlet } from 'react-router-dom';
 import React from 'react';
 import FamilyTree from './components/Tree/mytree';
 import Header from '../src/components/Header/index';
@@ -8,21 +9,6 @@ import Footer from '../src/components/Footer/index';
 import Homepage from '../src/pages/Homepage'
 
 //NOT SURE WHY THIS IS HERE; WHEN I COMMENT IT OUT, THE APP BREAKS, BUT WHEN I ADD ANOTHER PARAMETER TO PULL (LIKE BIRTHPLACE), NOTHING IN THE APP CHANGES
-// Define the GET_PERSONS query
-const GET_PERSONS = gql`
-  query GetPersons {
-    persons {
-      id
-      firstName
-      middleName
-      lastName
-      gender
-      img
-      fatherId
-      motherId
-    }
-  }
-`;
 
 // // Define the Apollo Client
 // const client = new ApolloClient({
@@ -30,26 +16,7 @@ const GET_PERSONS = gql`
 //   cache: new InMemoryCache(),
 // });
 
-// Create a separate component for querying and displaying data
-function Persons() {
-  const { loading, error, data } = useQuery(GET_PERSONS);
 
-  // Handle loading and error states
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
-//I'M NOT SURE WHY THIS IS HERE; IT DOESN'T SEEM TO DO ANYTHING, BUT WHEN I COMMENT IT OUT, THE APP BREAKS. WHEN I COMMENT OUT THE PARAMETERS, THE APP STILL WORKS
-  // Map the fetched data to the format required by the FamilyTree component
-  const nodes = data.persons.map(person => ({
-    id: person.id,
-    pids: [person.fatherId, person.motherId].filter(pid => pid), // Parent IDs
-    name: `${person.firstName} ${person.lastName}`,
-    gender: person.gender,
-    img: person.img || 'https://example.com/default-image.jpg', // Use the person's image or provide a default
-  }));
-
-  return <FamilyTree nodes={nodes} />;
-}
 
 
 const httpLink= createHttpLink ({
@@ -75,7 +42,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
     <Header /> 
-    <FamilyTree />
+    <Outlet />
     <Footer /> 
     </ApolloProvider>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,24 +1,50 @@
+import { ApolloClient, ApolloProvider, InMemoryCache, useQuery, gql,createHttpLink } from '@apollo/client';
 import ReactDOM from 'react-dom/client';
+import {setContext} from '@apollo/client/link/context'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 
 import App from './App.jsx';
-import Tree from './components/Tree/mytree.jsx'
+import FamilyTree from './components/Tree/mytree'
+import HomePage from './pages/Homepage.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <HomePage />,
   },
   {
     path: '/Tree',
-    element: <Tree/>
+    element: <FamilyTree />
   }
   
 ]);
 
+
+const httpLink= createHttpLink ({
+  uri:'/graphql'
+})
+
+const authLink = setContext((_,{headers}) => {
+  const token = localStorage.getItem('id_token')
+  return {
+    headers: {
+      ...headers,
+      authorization:token ? `bearer ${token}`: '',
+    }
+  }
+})
+
+
+const client=new ApolloClient({
+  link:authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+})
+
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
   <RouterProvider router={router} />
+  </ApolloProvider>
 );
 
 // //THIS IS THE ADAPTATION THAT CLAUDE.AI SUGGESTS; IT SEEMS TO BE CLEANER, BUT IT INSERTS THE TREE BELOW THE LOGIN PAGE
