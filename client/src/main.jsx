ad2cd559cb1e125f3f04faac5f70d226736a1b03
Matthew Williams <mattthewwilliamscmh@icloud.com>
2024-10-16 20:17:22 -0400

    some styling changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,8 @@ import {setContext} from '@apollo/client/link/context'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 
+import Header from "./components/Header/index.jsx"
+import Footer from "./components/Footer/index.jsx"
 import App from './App.jsx';
 import FamilyTree from './components/Tree/mytree'
 import HomePage from './pages/Homepage.jsx';
@@ -41,6 +43,8 @@ const client=new ApolloClient({
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ApolloProvider client={client}>
+  <Header />
   <RouterProvider router={router} />
+  <Footer />
   </ApolloProvider>
 );
