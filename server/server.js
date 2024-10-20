bb5e8be64deeeeac46ce5c68cf5df41ca741aa4d
MatthewWilliamsCMH <162186181+MatthewWilliamsCMH@users.noreply.github.com>
2024-10-20 19:59:38 -0400

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,8 +12,7 @@ const db = require('./config/connection');
 const User = require('./models/User'); // Adjust path as per your structure
 require('dotenv').config();
 
-const PORT = process.env.PORT || 10000;
-// const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001;j
 const app = express();
 app.use(cors());
 
